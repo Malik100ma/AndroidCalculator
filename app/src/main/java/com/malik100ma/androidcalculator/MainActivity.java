99450536f84b255b225d7355539794ee341b0e9e
Malik <1malik1chen1@gmail.com>
2026-08-11 19:23:59 +0300

    all

--- app/src/main/java/com/malik100ma/androidcalculator/MainActivity.java
+++ app/src/main/java/com/malik100ma/androidcalculator/MainActivity.java
@@ -2,7 +2,6 @@ package com.malik100ma.androidcalculator;
 
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +9,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvDisplay;
-    private boolean bNewNumber = true;
-    private double firstNumber = 0;
+    private boolean newNumber = true;
 
-    private String sOperation = "";
+
+    private TextView tvDisplay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +25,9 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
-
         tvDisplay = findViewById(R.id.textView);
-
-        findViewById(R.id.btn0).setOnClickListener(v -> addDigit("0"));
         findViewById(R.id.btn1).setOnClickListener(v -> addDigit("1"));
         findViewById(R.id.btn2).setOnClickListener(v -> addDigit("2"));
         findViewById(R.id.btn3).setOnClickListener(v -> addDigit("3"));
@@ -42,129 +37,63 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.btn7).setOnClickListener(v -> addDigit("7"));
         findViewById(R.id.btn8).setOnClickListener(v -> addDigit("8"));
         findViewById(R.id.btn9).setOnClickListener(v -> addDigit("9"));
+        findViewById(R.id.btn0).setOnClickListener(v -> addDigit("0"));
         findViewById(R.id.btnC).setOnClickListener(v -> clearAll());
         findViewById(R.id.btnDel).setOnClickListener(v -> clearDigit());
         findViewById(R.id.btnPoint).setOnClickListener(v -> addPoint());
-        findViewById(R.id.btnPlus).setOnClickListener(v -> setOperation("+"));
-        findViewById(R.id.btnMin).setOnClickListener(v -> setOperation("-"));
-        findViewById(R.id.btnMult).setOnClickListener(v -> setOperation("*"));
-        findViewById(R.id.btnDiv).setOnClickListener(v -> setOperation("/"));
-        findViewById(R.id.btnEq).setOnClickListener(v -> calculate());
-
-    }
-
-    private void calculate() {
-        double secondNumber = Double.parseDouble(tvDisplay.getText().toString());
-        double result = 0;
-        switch (sOperation) {
-            case "+":
-                result = firstNumber + secondNumber;
-                break;
-            case "-":
-                result = firstNumber - secondNumber;
-                break;
-            case "*":
-                result = firstNumber * secondNumber;
-                break;
-            case "/":
-                if (secondNumber == 0) {
-                    Toast.makeText(this, R.string.divNull, Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                result = firstNumber / secondNumber;
-                break;
-        }
-        if (result == (long) result){
-            tvDisplay.setText(String.valueOf((long) result));
-        }else {
-            tvDisplay.setText(String.valueOf(result));
-        }
-        setTextSize();
-        bNewNumber = true;
-    }
-
-    private void setOperation(String operation) {
-        double currentNumber = Double.parseDouble(tvDisplay.getText().toString());
-        if (operation.isEmpty() && !bNewNumber) {
-            switch (operation) {
-                case "+":
-                    firstNumber += currentNumber;
-                    break;
-                case "*":
-                    firstNumber *= currentNumber;
-                    break;
-                case "-":
-                    firstNumber -= currentNumber;
-                    break;
-                case "/":
-                    if (currentNumber == 0) {
-                        Toast.makeText(this, R.string.divNull, Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-                    firstNumber/= currentNumber;
-                    break;
-            }
-            if (firstNumber == (long) firstNumber){
-                tvDisplay.setText(String.valueOf((long) firstNumber));
-            }else {
-                tvDisplay.setText(String.valueOf(firstNumber));
-            }
-
-        } else {
-            firstNumber = Double.parseDouble(tvDisplay.getText().toString());
-        }
-
-        sOperation = operation;
-        bNewNumber = true;
     }
 
     private void addPoint() {
         String text = tvDisplay.getText().toString();
 
-        if (bNewNumber) {
+        if (newNumber) {
             tvDisplay.setText("0.");
-            bNewNumber = false;
+            newNumber = false;
         } else if (!text.contains(".")) {
             tvDisplay.append(".");
         }
-
     }
 
-    private void clearDigit() {
-        if (bNewNumber) {
+    public void clearDigit() {
+        if (newNumber == true) {
             return;
         }
-
         String text = tvDisplay.getText().toString();
-
         if (text.length() > 1) {
             tvDisplay.setText(text.substring(0, text.length() - 1));
         } else {
+            newNumber = true;
             tvDisplay.setText("0");
-            bNewNumber = true;
         }
+
+
     }
 
-    private void clearAll() {
-        bNewNumber = true;
+    public void clearAll() {
+        newNumber = true;
         tvDisplay.setText("0");
         setTextSize();
     }
 
-    private void addDigit(String number) {
-        setTextSize();
-        if (bNewNumber) {
-            tvDisplay.setText(number);
-            bNewNumber = false;
+    public void addDigit(String digit) {
+        if (newNumber) {
+            tvDisplay.setText(digit);
+            newNumber = false;
         } else {
-            tvDisplay.append(number);
+            tvDisplay.append(digit);
+        }
+        if (tvDisplay.length() > 8) {
+            tvDisplay.setTextSize(32);
+        } else {
+            tvDisplay.setTextSize(64);
         }
     }
 
     private void setTextSize() {
-        if (tvDisplay.length() > 9) {
+        if (tvDisplay.length() > 8) {
             tvDisplay.setTextSize(32);
-        } else
+        } else {
             tvDisplay.setTextSize(64);
+        }
     }
 }
